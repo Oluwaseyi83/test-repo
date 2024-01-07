fc2d2d4c66befde72ce967eceb724788c575373b
Oluwaseyi83 <98097890+Oluwaseyi83@users.noreply.github.com>
2024-01-06 23:36:34 -0700

    Update HomeController.java

--- SimpleWebApp/src/main/java/com/example/controller/HomeController.java
+++ SimpleWebApp/src/main/java/com/example/controller/HomeController.java
@@ -1,1 +1,15 @@
+// src/main/java/com/example/controller/HomeController.java
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello, this is a simple web application!";
+    }
+}
 
